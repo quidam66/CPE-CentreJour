f6d976110bf0c6f87f17651d6e47a4de77eaacf9
Denis Archambault <archiedenis@hotmail.com>
2016-05-08 18:42:46 -0400

    Nav bar
    debug and adjust it

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var _subItemSelected = false;
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation;
@@ -41,3 +42,69 @@ var ph = {
 }
 
 ph.init();
+
+$(".nav a").on("click", function(){
+	/*if($(this).parent().hasClass("active"))
+	{
+		console.log("dans le IF");
+		$(this).parent().addClass("active");
+		$(".nav").find(".active").removeClass("active");
+
+	}
+	else
+	{
+		console.log("dans le ELSE");
+		$(this).parent().addClass("active");		
+	}*/
+	if($(this).parent().hasClass("dropdown"))
+	{
+		
+		if(!$(this).parent().hasClass("active") && !_subItemSelected)
+		{
+			console.log("dans le IF du IF");
+			$(".nav").find(".active").removeClass("active");
+			$(this).parent().addClass("active");
+		}
+		else if(!$(this).parent().hasClass("active") && _subItemSelected)
+		{
+			console.log("dans le ELSE IF du IF");
+			$(this).parent().removeClass("active");
+			
+		}
+		else
+		{
+			console.log("dans le ELSE du IF");
+			//$(this).parent().addClass("active");
+		}
+		
+		//$(this).parent().addClass("selected");
+		
+	}
+	else
+	{
+		if($(this).parent().parent().hasClass("subitem"))
+		{
+			console.log("dans le SUBITEM");
+			if(_subItemSelected)
+			{
+				console.log("dans le IF du IF du ELSE");
+				$(".nav").find(".active").removeClass("active");
+				$(".nav").find(".selected").removeClass("selected");
+				$(this).parent().parent().parent().addClass("active");
+				
+			}
+			$(this).parent().addClass("active");
+			$(this).parent().parent().parent().addClass("selected");
+			_subItemSelected = true;
+		}
+		else
+		{
+			console.log("dans le ELSE du ELSE");
+			$(".nav").find(".active").removeClass("active");
+			$(this).parent().addClass("active");
+			_subItemSelected = false;
+		}
+
+		
+	}
+});
