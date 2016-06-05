2c0d3732f8d7e7cbbd39a65bf7f7486cceb2ca14
Denis Archambault <archiedenis@hotmail.com>
2016-06-05 18:27:29 -0400

    suite et fin de la section urgence

--- private/urgence.php
+++ private/urgence.php
@@ -1,38 +1,36 @@
 <?php
    include('session.php');
+
+	$json = file_get_contents("message.json");
+
+	//$parsed_json = json_decode($json);
+
+	$data = json_decode($json, true);
+	//$message = $parsed_json->{'message'}->{'texte'};
+	$message = $data['message']['texte'];
+	echo "Message: $message";
+
+	$error = "";
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+	// username and password sent from form 
+      	$data['message']['texte'] = $_POST['texte'];
+      	if(isset($_POST['affiche']))
+      	{
+      		$data['message']['active'] = true;
+      	}
+      	else
+      	{
+      		$data['message']['active'] = false;
+      	}
+		
+		$newJsonString = json_encode($data);
+		file_put_contents('message.json', $newJsonString);      
+   }
 ?>
 
 <!DOCTYPE html>
-<html>
-	<body>
-URGENCE
-		<h2 style="background-color:red">
-		Background-color set by using red
-		</h2>
 
-		<h2 style="background-color:orange">
-		Background-color set by using orange
-		</h2>
-
-		<h2 style="background-color:yellow">
-		Background-color set by using yellow
-		</h2>
-
-		<h2 style="background-color:blue;color:white">
-		Background-color set by using blue
-		</h2>
-
-		<h2 style="background-color:cyan">
-		Background-color set by using cyan
-		</h2>
-
-	</body>
-</html>
-
-
-
-
-<!doctype html>
 <html class="no-js" lang="fr-CA">
 <head>
     <meta charset="utf-8" />
@@ -47,37 +45,19 @@ URGENCE
 	<script src="../bootstrap/js/bootstrap.js"></script>
 	<script type = "text/javascript" language = "javascript">
 		$(document).ready(function() {
+           $.getJSON('message.json', function(jd) {
+           		console.log(jd.message.active)
+           		$('#affiche').prop("checked", jd.message.active);
 
-				
-               $.getJSON('message.json', function(jd) {
-               		console.log(jd.message.active)
-               		if(jd.message.active == "0")
-               		{
-               			$('#affiche').prop("checked", false);
-               		}
-               		else
-               		{
-               			$('#affiche').prop("checked", true);
-               		}
-
-
-                 /* $('#stage').html('<p> Name: ' + jd.name + '</p>');
-                  $('#stage').append('<p>Age : ' + jd.age+ '</p>');
-                  $('#stage').append('<p> Sex: ' + jd.sex+ '</p>');*/
-               });
-
-
-
-
-
-
-
-
-
-			$("#driver").click(function(event){
-				var texte = $("#texte").val();
-				$("#stage").load('/result.php', {"texte":texte} );
-			});
+           		if(jd.message.texte == "")
+           		{
+           			$('#json-texte').append('<p class="form-input">Aucun message à afficher</p>');
+           		}
+           		else
+           		{
+           			$('#json-texte').append('<p class="form-input">' + jd.message.texte + '</p>');
+           		}
+           });
 		});	
 	</script>
 </head>
@@ -85,19 +65,33 @@ URGENCE
    <div class="container">
       <div class="iframe-p-text">
          <div class="iframe-title-bg"><span>Section réservée à l'administration</span></div>
-         <div class="texteInfo">
-				<p>Enter your name and click on the button:</p>
-				<input type="checkbox" id="affiche" name="affiche" value=""/>Afficher le message<br>
-				<input type="input" id="texte" size="40" /><br />
+         	<div class="texteInfo">
+            	<div class="box-title">Entrez le message</div>
+	            <div class="box-inner">
+	            	<form class="message-form" id="formulaire" name="formulaire" action="" method="POST">
+			         	<div>
+							<span class="form-label">Afficher le message:</span>
+							<input type="checkbox" id="affiche" name="affiche[]" value="message">
+						</div>
 
-				<div id = "stage" style = "background-color:cc0;">
-				 STAGE
+						<div class="form-label">Dernier message enregistré:&nbsp;</div>
+						<div id="json-texte"></div>
+
+						<div class="form-label">Votre message:&nbsp;</div>
+						<div>
+							<textarea id="texte form-input" name="texte" value="" rows="3" cols="120"></textarea>
+						</div>
+						<div class="small-height"></div>
+						<div>
+						  	<button type="button submit" class="btn btn-primary">Envoyer</button>
+						</div>
+					</form>
 				</div>
-
-				<input type = "button" id = "driver" value = "Show Result" />
 			</div>
 		</div>
 	</div>
+
+	<div class="private-site-footer"></div> 
 </body>
 	
 </html>
